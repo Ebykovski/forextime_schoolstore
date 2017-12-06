2cb87bc00ad5ba19c5c80938ed140f1ddcc9aa6a
Быковский Евгений <evgeniy.bykovski@gmail.com>
2017-12-06 10:01:23 +0300

    Add Model & Mapper for Options

---
+++ src/models/Option.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Model;
+
+/**
+ * Option
+ *
+ * @author ebykovski
+ */
+final class Option extends BaseModel
+{
+    /**
+     * @var integer
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $name;
+
+    /**
+     * @var string
+     */
+    private $option_value;
+
+    /**
+     * Get Id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Option
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get option value
+     *
+     * @return string
+     */
+    public function getValue()
+    {
+        return $this->option_value;
+    }
+
+    /**
+     * Set option value
+     *
+     * @param string $option_value
+     * @return Option
+     */
+    public function setValue($option_value)
+    {
+        $this->option_value = $option_value;
+
+        return $this;
+    }
+
+}
---
+++ src/models/OptionMapper.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Model;
+
+/**
+ * OptionMapper
+ *
+ * @author ebykovski
+ */
+final class OptionMapper extends BaseMapper
+{
+
+    /**
+     * Name of table
+     *
+     * @var string
+     */
+    protected $tableName = 'options';
+
+    /**
+     * Name of model
+     *
+     * @var string
+     */
+    protected $modelName = '\App\Model\Option';
+
+}
